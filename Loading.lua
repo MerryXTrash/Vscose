5cbe3e6a76c611e3a9d2a4288dee44b6f6606417
x3Catsusz <158755815+MerryXTrash@users.noreply.github.com>
2024-09-03 19:23:00 +0700

    Update Loading.lua

--- Loading.lua
+++ Loading.lua
@@ -1,6 +1,7 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+local Show = loadstring(game:HttpGet("https://raw.githubusercontent.com/MerryXTrash/Vscose/main/Toggle.lua"))()
 
 local Window = Fluent:CreateWindow({
     Title = "Xervice HUB Beta",
